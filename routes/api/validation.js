9a31f99c4c8be58ee5a0066c5bf8676831b42f68
Mariia Oksova <oksova027@gmail.com>
2021-02-26 00:09:14 +0200

    Add express library

---
+++ routes/api/validation.js
@@ -0,0 +1,44 @@
+const Joi = require('joi')
+
+const schemaCreateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .required(),
+  phone: Joi.number().integer().min(7).required(),
+})
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net'] },
+    })
+    .required(),
+  phone: Joi.number().integer().min(7),
+})
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj)
+  if (error) {
+    const [{ message }] = error.details
+    return next({
+      status: 400,
+      message: `Filed: ${message.replace(/"/g, '')}`,
+      data: 'Bad request',
+    })
+  }
+  next()
+}
+
+module.exports.createContact = (req, res, next) => {
+  return validate(schemaCreateContact, req.body, next)
+}
+
+module.exports.updateContact = (req, res, next) => {
+  return validate(schemaUpdateContact, req.body, next)
+}
